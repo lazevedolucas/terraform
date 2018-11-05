1665cfd72175432abfdccff518b98ef301883753
lazevedolucas <lazevedo.lucas@gmail.com>
2018-11-05 19:01:02 -0200

    Lab02 commit inicial

---
+++ LAB02/README.md
@@ -0,0 +1,2 @@
+# Lab02
+Criação de 1 VPC, 2 Subnet (1 Publica e 1 Privada), 1 Security Group, 1 Instância usando estes recursos em cada subnet.
---
+++ LAB02/main.tf
@@ -0,0 +1,92 @@
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = "/home/lazevedo/.aws/credentials"
+  profile                 = "default"
+}
+
+resource "aws_vpc" "vpc_modelo" {
+  cidr_block = "10.200.0.0/16"
+  enable_dns_hostnames = true
+  tags {
+    name = "vpc_modelo"
+  }
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.vpc_modelo.id}"
+  tags  {
+    name = "vpc_modelo"
+  }
+}
+
+resource "aws_route_table" "subnet_pub1" {
+  vpc_id  = "${aws_vpc.vpc_modelo.id}"
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id  = "${aws_internet_gateway.vpc_modelo.id}"
+    }
+    tags {
+      name = "vpc_modelo"
+  }
+}
+
+resource "aws_subnet" "subnet_pub1" {
+  vpc_id = "${aws_vpc.vpc_modelo.id}"
+  cidr_block = "10.200.1.0/24"
+  availability_zone = "us-east-1a"
+  tags {
+    name = "subnet_pub1"
+  }
+}
+
+resource "aws_route_table_association" "subnet_pub1" {
+  subnet_id = "${aws_subnet.subnet_pub1.id}"
+  route_table_id = "${aws_route_table.subnet_pub1.id}"
+}
+
+resource "aws_subnet" "subnet_priv1" {
+  vpc_id = "${aws_vpc.vpc_modelo.id}"
+  cidr_block = "10.200.2.0/24"
+  availability_zone = "us-east-1a"
+  tags {
+    name = "subnet_priv1"
+  }
+}
+
+resource "aws_instance" "ec2_subnet_pub1" {
+  ami = "ami-0ac019f4fcb7cb7e6"
+  instance_type = "t2.micro"
+  vpc_security_group_ids  = ["${aws_security_group.ssh_only.id}"]
+  subnet_id = "${aws_subnet.subnet_pub1.id}"
+  associate_public_ip_address = "true"
+}
+
+resource "aws_instance" "ec2_subnet_priv1" {
+  ami = "ami-0ac019f4fcb7cb7e6"
+  instance_type = "t2.micro"
+  vpc_security_group_ids  = ["${aws_security_group.ssh_only.id}"]
+  subnet_id = "${aws_subnet.subnet_priv1.id}"
+  associate_public_ip_address = "false"
+}
+
+resource "aws_security_group" "ssh_only" {
+  name  = "ssh_only"
+  description = "Allow SSH Only to All"
+  vpc_id = "${aws_vpc.vpc_modelo.id}"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags {
+    name= "ssh_only"
+  }
+}
